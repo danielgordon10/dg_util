a1c9f52f89dd811001b86af1bb9af231444d0232
Daniel Gordon <xkcd@cs.washington.edu>
2019-05-14 18:11:10 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dg_util',
+    version='1.0',
+    packages=['python'],
+    url='',
+    license='',
+    author='Daniel Gordon',
+    author_email='',
+    description=''
+)
