38b74c0d5b995bb69ab98cc7c9bc9003068296c5
Daniel Gordon <xkcd@cs.washington.edu>
2019-12-17 11:10:03 -0800

    made rolling average O(1)

--- dg_util/python_utils/average_meter.py
+++ dg_util/python_utils/average_meter.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 import torch
+from . import pytorch_util as pt_util
 
 
 class AverageMeter(object):
@@ -21,11 +22,11 @@ class AverageMeter(object):
         self.count = 0
 
     def update(self, val, n=1):
-        with torch.no_grad():
-            self.val = val
-            self.sum += val * n
-            self.count += n
-            self.avg = self.sum / self.count
+        val = pt_util.to_numpy(val)
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 class RollingAverageMeter(object):
@@ -35,15 +36,25 @@ class RollingAverageMeter(object):
         self.deque = None
         self.val = None
         self.avg = None
+        self.sum = None
         self.reset()
 
     def reset(self):
         self.deque = deque(maxlen=self.window_size)
         self.val = 0
         self.avg = 0
+        self.sum = 0
 
     def update(self, val):
-        with torch.no_grad():
-            self.deque.append(val)
-            self.val = val
-            self.avg = sum(self.deque) / len(self.deque)
+        val = pt_util.to_numpy(val)
+        if len(self.deque) == self.window_size:
+            prev_val = self.deque.popleft()
+            diff = val - prev_val
+            self.sum += diff
+            self.avg += diff / len(self.deque)
+        else:
+            self.sum += val
+            self.avg = self.sum / len(self.deque)
+
+        self.deque.append(val)
+        self.val = val
