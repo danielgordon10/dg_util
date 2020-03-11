3da135560b09839212b9712aa06128f1922c82eb
Daniel Gordon <xkcd@cs.washington.edu>
2020-03-11 12:08:35 -0700

    updated average meter

--- dg_util/python_utils/average_meter.py
+++ dg_util/python_utils/average_meter.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 import torch
-from . import pytorch_util as pt_util
+from .misc_util import CircularBuffer
 
 
 class AverageMeter(object):
@@ -22,39 +22,34 @@ class AverageMeter(object):
         self.count = 0
 
     def update(self, val, n=1):
-        val = pt_util.to_numpy(val)
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+        with torch.no_grad():
+            self.val = val
+            self.sum += val * n
+            self.count += n
+            self.avg = self.sum / self.count
 
 
 class RollingAverageMeter(object):
     def __init__(self, window_size=10):
         super(RollingAverageMeter, self).__init__()
         self.window_size = window_size
-        self.deque = None
+        self.circular_buffer = None
         self.val = None
         self.avg = None
-        self.sum = None
+        self.total = None
         self.reset()
 
     def reset(self):
-        self.deque = deque(maxlen=self.window_size)
+        self.circular_buffer = CircularBuffer(self.window_size)
         self.val = 0
         self.avg = 0
-        self.sum = 0
+        self.total = 0
 
     def update(self, val):
-        val = pt_util.to_numpy(val)
-        if len(self.deque) == self.window_size:
-            prev_val = self.deque.popleft()
-            diff = val - prev_val
-            self.sum += diff
-            self.avg += diff / (len(self.deque) + 1)
-        else:
-            self.sum += val
-            self.avg = self.sum / (len(self.deque) + 1)
-
-        self.deque.append(val)
-        self.val = val
+        with torch.no_grad():
+            prev_val = self.circular_buffer.append(val)
+            self.val = val
+            if prev_val is not None:
+                self.total -= prev_val
+            self.total += val
+            self.avg = self.total / len(self.circular_buffer)
