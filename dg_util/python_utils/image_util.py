823e25acfe20fbc948baeaf9f3f66b2f6570c402
Daniel Gordon <xkcd@cs.washington.edu>
2020-03-19 18:27:54 -0700

    a bit of refactoring

--- dg_util/python_utils/image_util.py
+++ dg_util/python_utils/image_util.py
@@ -1,9 +1,11 @@
+import numbers
+
 import cv2
 import imagesize
-from PIL import Image
 import numpy as np
-import scipy
-import numbers
+from skimage.draw import circle
+
+from . import misc_util
 
 
 # @inputImage{ndarray HxWx3} Full input image.
@@ -74,4 +76,91 @@ def get_cropped_input(inputImage, bbox, padScale, outputSize, interpolation=cv2.
 
 def get_image_size(fname):
     width, height = imagesize.get(fname)
-    return image.size
+    return width, height
+
+
+def draw_images_at_locations(images, x_coords, y_coords, img_res=64, res=4000, background_color=255, labels=None, point_radius=20):
+    """
+    Plots images at the provided coordinates.
+
+    Parameters
+    ---------
+    images: list or numpy array
+        Images to draw.
+
+    x_coords: list or numpy array
+        x coordinates of where to draw each image.
+
+    y_coords: list or numpy array
+        y coordinates of where to draw each image.
+
+    img_res: int
+        Resolution to draw each images.
+
+    res: int
+        Size of full image in pixels.
+
+    background_color: float or numpy array
+        Background color value.
+
+    labels: List or numpy array if provided
+        Label for each image for drawing circle image.
+
+    point_radius: int
+        Size of the circle for the label image.
+
+    """
+    images = np.asarray(images)
+    x_coords = np.asarray(x_coords)
+    y_coords = np.asarray(y_coords)
+    n, h, w, c = images.shape
+    assert x_coords.shape[0] == y_coords.shape[0] == n
+    images = misc_util.min_resize(images.transpose(1, 2, 0, 3).reshape(h, w, n * c), img_res)
+    images = images.reshape(images.shape[0], images.shape[1], n, c).transpose(2, 0, 1, 3)
+
+    max_width = max(img_res, point_radius * 2)
+    max_height = max(img_res, point_radius * 2)
+    if x_coords.max() - x_coords.min() > y_coords.max() - y_coords.min():
+        temp = x_coords
+        x_coords = y_coords
+        y_coords = temp
+    x_min, x_max = x_coords.min(), x_coords.max()
+    y_min, y_max = y_coords.min(), y_coords.max()
+    # Fix the ratios
+    sx = x_max - x_min
+    sy = y_max - y_min
+    if sx > sy:
+        res_x = sx / float(sy) * res
+        res_y = res
+    else:
+        res_x = res
+        res_y = sy / float(sx) * res
+
+    canvas = np.full((int(res_x + max_width), int(res_y + max_height), 3), background_color, dtype=np.uint8)
+    if labels is not None:
+        circles = np.full(canvas.shape, 255, dtype=np.uint8)
+        label_classes = np.unique(labels)
+    x_coords = np.floor((x_coords - x_min) / (x_max - x_min) * res_x).astype(np.int64)
+    y_coords = np.floor((y_coords - y_min) / (y_max - y_min) * res_y).astype(np.int64)
+    im_ind = 0
+    for ii, x_idx, y_idx, image in zip(range(len(x_coords)), x_coords, y_coords, images):
+        w, h = image.shape[:2]
+        if labels is not None:
+            center = (int(y_idx + h / 2.0), int(x_idx + w / 2.0))
+            rr, cc = circle(center[1], center[0], point_radius)
+            label = np.where(label_classes == labels[ii])[0].item()
+            color = cv2.applyColorMap(
+                np.array(int(label * 255.0 / len(label_classes)), dtype=np.uint8), cv2.COLORMAP_JET
+            ).squeeze()
+            circles[rr, cc, :] = color
+
+        canvas[x_idx : x_idx + w, y_idx : y_idx + h] = image
+        im_ind += 1
+
+    images = list()
+    images.append(canvas)
+
+    if labels is not None:
+        images.append(circles)
+
+    return images
--- dg_util/python_utils/tsne.py
+++ dg_util/python_utils/tsne.py
@@ -1,14 +1,11 @@
 import multiprocessing
 import time
 
-import cv2
 import numpy as np
 from MulticoreTSNE import MulticoreTSNE as TSNE
-from PIL import Image
-from skimage.draw import circle
 from sklearn.decomposition import PCA
 
-from . import misc_util
+from . import image_util
 
 try:
     from StringIO import StringIO  # Python 2.7
@@ -17,7 +14,7 @@ except ImportError:
 
 
 def tsne_image(
-    features, images, img_res=64, res=4000, cval=255, point_radius=20, max_feature_size=-1, labels=None, n_threads=0
+    features, images, img_res=64, res=4000, background_color=255,  max_feature_size=-1, labels=None, point_radius=20, n_threads=0
 ):
     """
     Embeds images via tsne into a scatter plot.
@@ -36,23 +33,27 @@ def tsne_image(
     res: int
         Size of embedding image in pixels
 
-    cval: float or numpy array
+    background_color: float or numpy array
         Background color value
+
+    max_feature_size: int
+        If input_feature_size > max_feature_size> 0, features are first
+        reduced using PCA to the desired size.
+
+    point_radius: int
+        Size of the circle for the label image.
+
+    n_threads: int
+        Number of threads to use for t-SNE
+
+
+    labels: List or numpy array if provided
+        Label for each image for drawing circle image.
+
 
     """
     features = np.asarray(features, dtype=np.float32)
-    images = np.asarray(images)
     assert len(features.shape) == 2
-    #_, uniques = misc_util.unique_rows(images.reshape(images.shape[0], -1), return_inverse=True)
-    #uniques.sort()
-    #images = images[uniques]
-    #features = features[uniques]
-    n, h, w, c = images.shape
-    images = misc_util.min_resize(images.transpose(1, 2, 0, 3).reshape(h, w, n * c), img_res)
-    images = images.reshape(images.shape[0], images.shape[1], n, c).transpose(2, 0, 1, 3)
-
-    max_width = max(img_res, point_radius * 2)
-    max_height = max(img_res, point_radius * 2)
 
     print("Starting TSNE")
     s_time = time.time()
@@ -68,49 +69,9 @@ def tsne_image(
     print("TSNE done.", (time.time() - s_time))
     print("Starting drawing.")
 
-    xx = f2d[:, 0]
-    yy = f2d[:, 1]
-    if xx.max() - xx.min() > yy.max() - yy.min():
-        temp = xx
-        xx = yy
-        yy = temp
-    x_min, x_max = xx.min(), xx.max()
-    y_min, y_max = yy.min(), yy.max()
-    # Fix the ratios
-    sx = x_max - x_min
-    sy = y_max - y_min
-    if sx > sy:
-        res_x = sx / float(sy) * res
-        res_y = res
-    else:
-        res_x = res
-        res_y = sy / float(sx) * res
+    x_coords = f2d[:, 0]
+    y_coords = f2d[:, 1]
 
-    canvas = np.full((int(res_x + max_width), int(res_y + max_height), 3), cval, dtype=np.uint8)
-    if labels is not None:
-        circles = np.full(canvas.shape, 255, dtype=np.uint8)
-        label_classes = np.unique(labels)
-    xx = np.floor((xx - x_min) / (x_max - x_min) * res_x).astype(np.int64)
-    yy = np.floor((yy - y_min) / (y_max - y_min) * res_y).astype(np.int64)
-    im_ind = 0
-    for ii, x_idx, y_idx, image in zip(range(len(xx)), xx, yy, images):
-        w, h = image.shape[:2]
-        if labels is not None:
-            center = (int(y_idx + h / 2.0), int(x_idx + w / 2.0))
-            rr, cc = circle(center[1], center[0], point_radius)
-            label = np.where(label_classes == labels[ii])[0].item()
-            color = cv2.applyColorMap(
-                np.array(int(label * 255.0 / len(label_classes)), dtype=np.uint8), cv2.COLORMAP_JET
-            ).squeeze()
-            circles[rr, cc, :] = color
+    return image_util.draw_images_at_locations(images, x_coords, y_coords, img_res, res, background_color, labels, point_radius)
 
-        canvas[x_idx : x_idx + w, y_idx : y_idx + h] = image
-        im_ind += 1
 
-    images = list()
-    images.append(canvas)
-
-    if labels is not None:
-        images.append(circles)
-
-    return images
