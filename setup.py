fe750d4672cc4253481c0fb73565a8dc31260a36
Daniel Gordon <danielgordon10@gmail.com>
2020-04-03 11:44:17 -0700

    added more packagey files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="dg_util",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     install_requires=requirements,
     url="https://github.com/danielgordon10/dg_util",
